ba5d67d183bec79a64b5b45b453a9d19f0ac0ad8
anthonyc_36 <anthonychristy78@gmail.com>
2020-02-26 14:47:14 -0500

    Initial Commit

---
+++ AT_V_EAB/Ash Trees V EABs/Assets/Scripts/Health.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    //current health
+    [SerializeField] int cur = 5;
+
+    public void doDamage(int n)
+    {
+        //subtract damage from current health
+        cur -= n;
+
+        //destroy if dead
+        if (cur <= 5)
+            Destroy(gameObject);
+    }
+
+
+}
